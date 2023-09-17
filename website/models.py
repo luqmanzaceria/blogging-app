8d7d65dc65129c8d3d765f338603b434abd46e5e
Luqman Zaceria <luqmanzaceria@gmail.com>
2023-09-17 20:35:57 +0530

    Create database model for posts

--- website/models.py
+++ website/models.py
@@ -8,3 +8,8 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text, nullable=False)
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
