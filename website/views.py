c559955a92d7d5e78d1cb2d4671c52f53b7b9c2f
Luqman Zaceria <luqmanzaceria@gmail.com>
2023-09-24 18:59:36 +0530

    Redirect to home after post creation

--- website/views.py
+++ website/views.py
@@ -9,7 +9,8 @@ views = Blueprint("views", __name__)
 @views.route("/home")
 @login_required
 def home():
-    return render_template("home.html", user=current_user)
+    posts = Post.query.all()
+    return render_template("home.html", user=current_user, posts=posts)
 
 @views.route("create-post", methods=['GET', 'POST'])
 @login_required
@@ -24,5 +25,6 @@ def create_post():
             db.session.add(post)
             db.session.commit()
             flash('Post created!', category='success')
+            return redirect(url_for('views.home'))
 
     return render_template("create_post.html", user=current_user)
