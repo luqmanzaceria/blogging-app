a3d29972a8a6bb009683f87030966213e3113151
Luqman Zaceria <luqmanzaceria@gmail.com>
2023-09-24 19:31:33 +0530

    Add database model for comments

--- website/views.py
+++ website/views.py
@@ -54,5 +54,5 @@ def posts(username):
         flash('No user with that username exists.', category='error')
         return redirect(url_for('views.home'))
 
-    posts = Post.query.filter_by(author=user.id).all()
+    posts = user.posts
     return render_template("posts.html", user=current_user, posts=posts, username=username)
